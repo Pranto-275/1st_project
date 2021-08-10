7ad5a23cf24aa112284807716c4512ac2077e581
Pranto-275 <67626492+Pranto-275@users.noreply.github.com>
2021-08-10 12:01:15 +0600

    servicesection
    servicesection connect with database

--- site/app/Http/Controllers/HomeController.php
+++ site/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ServiceModel;
 use App\VisitorModel;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,10 @@ class HomeController extends Controller
         date_default_timezone_set("Asia/Dhaka");
         $timeDate = date("y-m-d h:i:sa");
         VisitorModel::insert(['ip_address' => $UserIP, 'Visit_time' => $timeDate]);
-        return view('Home');
+
+        $ServicesData = json_decode(ServiceModel::all());
+
+
+        return view('Home', ['ServicesData' => $ServicesData]);
     }
 }
---
+++ site/app/ServiceModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceModel extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public $table = 'Services';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    public $primaryKey = 'id';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    public $keyType = 'int';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
